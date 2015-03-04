db9aa5944fb8d3f61f082502315e48db446feaa4
unknown <jaakko.jokinen@outlook.com>
2015-03-04 13:24:20 +0200

    menu toimii jotenkin

--- MainMenu.java
+++ MainMenu.java
@@ -1,66 +1,62 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.*;
 
-public class MainMenu extends Canvas {
-	
+public class MainMenu {
+
 	private JFrame ikkuna;
-	public boolean peliKaynnissa;
-	
+	private static final JPanel P1 = new JPanel();
+
 	public final int LEVEYS = 1280;
 	public final int KORKEUS = LEVEYS / 16 * 9;
 	public final Dimension ruudunKoko = new Dimension(LEVEYS, KORKEUS);
 
 
-	public MainMenu(JFrame ikkuna) {
-		this.ikkuna = ikkuna;
-		setPreferredSize(ruudunKoko);
-		ikkuna.add(this, BorderLayout.CENTER);
-		ikkuna.pack();
-		ikkuna.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		ikkuna.setVisible(true);
+	public MainMenu(JFrame akkuna) {
+		this.ikkuna = akkuna;
+		
+		//p1.setLayout(new BoxLayout(ikkuna, 1280));
+		P1.setPreferredSize(new Dimension(400, 40));
+		P1.setBackground(Color.RED);
+		JButton b1 = new JButton("Yksinpeli");
+		JButton b2 = new JButton("Moninpeli");
+		JButton b3 = new JButton("Lataa peli");
+		JButton b4 = new JButton("Huipputulokset");
+
+		b1.addActionListener( new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				ikkuna.remove(P1);
+				Pong peli = new Pong(1, ikkuna);
+				peli.start();
+			}
+		});
+		b2.addActionListener( new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				ikkuna.remove(P1);
+				Pong peli = new Pong(0, ikkuna);
+				peli.start();
+			}
+		});
+		
+		P1.add(b1);
+		P1.add(b2);
+		P1.add(b3);
+		P1.add(b4);
+		ikkuna.add(P1);
+
+		ikkuna.setPreferredSize(ruudunKoko);
 		ikkuna.setResizable(false);
 		ikkuna.setTitle("Balamaui - Pong");
 		ikkuna.setBackground(Color.BLACK);
-		ikkuna.setLocationRelativeTo(null);
-		
-		/* JMenuBar jmb = new JMenuBar();
+		ikkuna.pack();
+		ikkuna.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		ikkuna.setVisible(true);
 
-	    JMenu jmFile = new JMenu("File");
-	    JMenuItem jmiStart = new JMenuItem("Start");
-	    JMenuItem jmiSave = new JMenuItem("Save");
-	    JMenuItem jmiExit = new JMenuItem("Exit");
-	    jmFile.add(jmiStart);
-	    jmFile.add(jmiSave);
-	    jmFile.addSeparator();
-	    jmFile.add(jmiExit);
-	    jmb.add(jmFile);
-
-	    JMenu jmHelp = new JMenu("Help");
-	    JMenuItem jmiAbout = new JMenuItem("About");
-	    jmHelp.add(jmiAbout);
-	    jmb.add(jmHelp);
-	    
-	    jmiStart.addActionListener(this);
-
-	    ikkuna.setJMenuBar(jmb);
-	    ikkuna.setVisible(true); */
-		
-		JPanel b1 = new JPanel();
-				
-		JButton c1 = new JButton("Disable middle button");
-		b1.add(c1);
-	    ikkuna.add(b1);
-		
-		this.peliKaynnissa = false;
 	}
-	
+
 	public static void main(String[] args) {
 		JFrame ikkuna = new JFrame();
 		MainMenu spela = new MainMenu(ikkuna);
-		
-		if (spela.peliKaynnissa) {
-			Pong peli = new Pong(0, ikkuna);
-			peli.start();
-		}
 	}
 }
