fea9b4d7171807a15295362e5c08c0018d210f1a
jammat <jammat@utu.fi>
2015-03-17 11:59:16 +0200

    jotain ai:n kehitystä, ei tarvi muokata enää

--- AIMaila.java
+++ AIMaila.java
@@ -7,21 +7,24 @@ public class AIMaila extends Maila {
 		super(x, y, leveys, korkeus);
 	}
 
+	static Random luku = new Random();
+	static int heitto = luku.nextInt(20);
+
 	@Override
 	public void liiku(GameOn g) {
 		this.rajat.setLocation(x, y);
+		
 		// jos pallo on oikealla kenttapuolella
-		if (g.pallo.x > 545 && g.pallo.x < 1000) {
-			if(g.pallo.getY() +10 <= y)
-				y -= vauhti + 1;
-			if(g.pallo.getY() >= y && y <= g.pallo.y - korkeus)
-				y += vauhti + 1;
+		if (g.pallo.x > 465 && g.pallo.x < 1000) {
+			if(g.pallo.getY() + heitto -5 <= y)
+				y -= vauhti;
+			if(g.pallo.getY() + heitto - 5 >= y && y <= g.pallo.y - korkeus)
+				y += vauhti;
 		}	
 		if (g.pallo.x > 1000 && g.pallo.x < 1216) {
-			if(g.pallo.getY() +10 <= y)
-				y -= vauhti + 2;
-			if(g.pallo.getY() >= y && y <= g.pallo.y - korkeus)
-				y += vauhti + 2;
+			if(g.pallo.getY() + heitto - 5 <= y)
+				y -= vauhti + 1;
+			if(g.pallo.getY() + heitto - 5 >= y && y <= g.pallo.y - korkeus)
+				y += vauhti + 1;
 		}
 	}	
-}
